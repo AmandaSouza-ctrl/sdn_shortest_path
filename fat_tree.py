7978e1eea7e5dce4190df20d8000b528df145847
Amanda Souza <55111387+AmandaSouza-ctrl@users.noreply.github.com>
2025-06-02 13:17:39 -0300

    Update fat_tree.py

--- fat_tree.py
+++ fat_tree.py
@@ -44,7 +44,7 @@ class Fattree(Topo):
     """
 
     def _addSwitch(self, number, level, switch_list):
-        for x in xrange(1, number+1):
+        for x in range(1, number+1):
             PREFIX = str(level) + "00"
             if x >= int(10):
                 PREFIX = str(level) + "0"
@@ -64,7 +64,7 @@ class Fattree(Topo):
 
     def createHost(self, NUMBER):
         logger.debug("Create Host")
-        for x in xrange(1, NUMBER+1):
+        for x in range(1, NUMBER+1):
             PREFIX = "h00"
             if x >= int(10):
                 PREFIX = "h0"
@@ -78,25 +78,25 @@ class Fattree(Topo):
     def createLink(self, bw_c2a=0.2, bw_a2e=0.1, bw_h2a=0.5):
         logger.debug("Add link Core to Agg.")
         end = self.pod/2
-        for x in xrange(0, self.iAggLayerSwitch, end):
-            for i in xrange(0, end):
-                for j in xrange(0, end):
+        for x in range(0, self.iAggLayerSwitch, end):
+            for i in range(0, end):
+                for j in range(0, end):
                     self.addLink(
                         self.CoreSwitchList[i*end+j],
                         self.AggSwitchList[x+i],
                         bw=bw_c2a)
 
         logger.debug("Add link Agg to Edge.")
-        for x in xrange(0, self.iAggLayerSwitch, end):
-            for i in xrange(0, end):
-                for j in xrange(0, end):
+        for x in range(0, self.iAggLayerSwitch, end):
+            for i in range(0, end):
+                for j in range(0, end):
                     self.addLink(
                         self.AggSwitchList[x+i], self.EdgeSwitchList[x+j],
                         bw=bw_a2e)
 
         logger.debug("Add link Edge to Host.")
-        for x in xrange(0, self.iEdgeLayerSwitch):
-            for i in xrange(0, self.density):
+        for x in range(0, self.iEdgeLayerSwitch):
+            for i in range(0, self.density):
                 self.addLink(
                     self.EdgeSwitchList[x],
                     self.HostList[self.density * x + i],
